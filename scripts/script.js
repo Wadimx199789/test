8d6f5e8b90442554d6fa519d138233086fd5598c
Wadim Martinkevich <martinkewichwadim@gmail.com>
2022-05-18 06:21:31 +0300

    fix: change scroll speed

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,15 @@ window.onload = function () {
     const arrLinks = document.querySelectorAll('.menu__link');
     const ScrollToBottomBtn = document.querySelector('.scroll-button');
 
-    ScrollToBottomBtn.addEventListener('click',function (e) {
-            
+    ScrollToBottomBtn.addEventListener('click', function () {
+
         scrollMenu(this.getAttribute('href'));
     })
 
-
     for (let i = 0; i < arrLinks.length; i++) {
 
-        arrLinks[i].addEventListener('click', function (e) {
-            
+        arrLinks[i].addEventListener('click', function () {
+
             scrollMenu(this.getAttribute('href'));
         })
 
@@ -21,19 +20,23 @@ window.onload = function () {
 
 // scroll function
 function scrollMenu(blockId) {
-    let temp;
+    let temp,start,from,to;
+
     cancelAnimationFrame(temp);
-    var start = performance.now();
-    var from = window.pageYOffset || document.documentElement.scrollTop,
+        start = performance.now();
+        from = window.pageYOffset || document.documentElement.scrollTop;
         to = document.querySelector(blockId).getBoundingClientRect().top - 33;
-    duration = 1000 * Math.abs(to) / 4000;
+        if (blockId === "#lab") {
+            duration = 1000 * Math.abs(to) / 1000;
+        } else
+            duration = 1000 * Math.abs(to) / 4000;
 
     // scroll
     requestAnimationFrame(function step(timestamp, e) {
         var progress = (timestamp - start) / duration;
         1 <= progress && (progress = 1);
         window.scrollTo(0, from + to * progress | 0);
-        (1 > progress) ? temp = requestAnimationFrame(step): (document.location.hash = blockId);
+        (1 > progress) ? temp = requestAnimationFrame(step) : (document.location.hash = blockId);
         document.addEventListener("wheel", function () {
             cancelAnimationFrame(temp);
         })
